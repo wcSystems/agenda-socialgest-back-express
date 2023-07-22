bd9c18bd346f78700f42b3217ed1f0d0d9752101
Anthony Willinthon Carriedo Peña <anthonycarriedo2013@gmail.com>
2023-07-22 15:57:04 -0400

    test con puerto default

--- migration.js
+++ migration.js
@@ -1,22 +1,23 @@
 var mysql = require('mysql');
 var migration = require('mysql-migrations');
 
-var connection = mysql.createPool({
+/* var connection = mysql.createPool({
    connectionLimit: 10,
    host: 'localhost',
    user: 'root',
    password: '',
    database: 'rest_api'
-});
+}); */
 
-/* var connection = mysql.createPool({
+var connection = mysql.createPool({
    connectionLimit: 10,
    host: 'ls-6aad697d8a7da588a86daf8023c6bc04fab93ad1.corq9qujxtx5.us-east-1.rds.amazonaws.com',
    user: 'dbmasteruser',
    password: '#LEur)S2WK)A(>;8kb~MAiT|YNe*iqZX',
    database: 'dbmaster'
 });
- */
+
+
 function executeQuery(sql, callback) {
    connection.getConnection((err, connection) => {
       if (err) {
